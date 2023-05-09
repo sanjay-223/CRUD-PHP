e82a0b058d887f320c50f7fd99d257c9051b7bad
Sanjay S <111056668+sanjay-223@users.noreply.github.com>
2023-05-09 08:34:56 +0530

    Create update.php

---
+++ update.php
@@ -0,0 +1,10 @@
+<?php
+  $conn = new mysqli("localhost", "root", "787898", "php_demo");
+  $id = $_POST['id'];
+  $name = $_POST['name'];
+  $score = $_POST['score'];
+  $sql = "update demo_table set name='$name', score='$score' where id=$id";
+  $result = $conn->query($sql);
+  $conn->close();
+  header("location: index.php");
+?>
